06e501f671f360b1618b3670f78b28658ed0739e
Sylwester Leyk <sylwester95leyk@gmail.com>
2023-01-24 13:02:40 +0100

    #1 Napisanie kodu programu wyświetlającego kody pocztowe Świnoujścia

--- App.java
+++ App.java
@@ -1,2 +1,39 @@
 class App{
+    public static void main(String args[]){
+        int[] postCode = new int[13];
+
+        postCode [0] = 72600;
+        postCode [1] = 72601;
+        postCode [2] = 72602;
+        postCode [3] = 72603;
+        postCode [4] = 72604;
+        postCode [5] = 72605;
+        postCode [6] = 72606;
+        postCode [7] = 72607;
+        postCode [8] = 72608;
+        postCode [9] = 72609;
+        postCode [10] = 72610;
+        postCode [11] = 72611;
+        postCode [12] = 72612;
+
+        System.out.println("-------------------------------------------");
+        System.out.println("Kody pocztowe dla miejscowości Świnoujście:");
+        System.out.println("* "+postCode[0]);
+        System.out.println("* "+postCode[1]);
+        System.out.println("* "+postCode[2]);
+        System.out.println("* "+postCode[3]);
+        System.out.println("* "+postCode[4]);
+        System.out.println("* "+postCode[5]);
+        System.out.println("* "+postCode[6]);
+        System.out.println("* "+postCode[7]);
+        System.out.println("* "+postCode[8]);
+        System.out.println("* "+postCode[9]);
+        System.out.println("* "+postCode[10]);
+        System.out.println("* "+postCode[11]);
+        System.out.println("* "+postCode[12]);
+        System.out.println("-------------------------------------------");
+
+
+
+    }
 }
